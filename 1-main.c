ece2a9e3258c8bccb2e7880a2620ef1a37919716
21Kgold <claudiarpa@gmail.com>
2022-05-15 21:15:22 -0700

    update

---
+++ 1-main.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+/**
+ * main - Monty interpreter main Startpoint
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 if success, or any other value if not successfull
+ */
+
+int main(int ac, char **av)
+{
+char *file = av[1];
+char *buffer = NULL;
+char **tokens = NULL;
+size_t len = 0;
+size_t li_numb = 1;
+int read_line;
+stack_t *st_stack = NULL;
+FILE *fd;
+
+if (ac != 2)
+	{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+	}
+else
+	{
+	fd = fopen(file, "r");
+	if (fd == NULL)
+		{
+		fprintf(stderr, "Error: Can't open file %s\n", file);
+		exit(EXIT_FAILURE);
+		}
+	for (li_numb = 1; (read_line = getline(&buffer, &len, fd)) != -1; li_numb++)
+		{
+		/*read_line = getline(&buffer, &len, fd);*/
+		tokens_line(&buffer, &tokens, li_numb);
+		if (tokens != NULL)
+			{
+			opcode_choose(&st_stack, &tokens, li_numb);
+			}
+		freetokens(&tokens);
+		}
+	fclose(fd);
+	if (buffer != NULL)
+		{
+		free(buffer);
+		}
+	freest_stack(st_stack);
+	}
+return (0);
+}
